1d485458f0a6b5468d157b1e17778b382ef5b34f
Andres Soca <lumen.development01@gmail.com>
2021-12-13 20:14:39 -0500

    created log in, logout, and auth js features and functionality - ASoca

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ const { sequelize } = require('./db/models');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const bcrypt = require('bcryptjs');
+const { sessionSecret } = require('./config');
+const { restoreUser } = require("./auth");
+
+// Route imports
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -18,7 +22,7 @@ app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser(sessionSecret));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // set up session middleware
@@ -26,21 +30,22 @@ const store = new SequelizeStore({ db: sequelize });
 
 app.use(
   session({
-    secret: 'superSecret',
+    secret: sessionSecret,
     store,
     saveUninitialized: false,
     resave: false,
   })
-);
+  );
 
-// create Session table if it doesn't already exist
-store.sync();
+  // create Session table if it doesn't already exist
+  store.sync();
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+  app.use(restoreUser);
+  app.use('/', indexRouter);
+  app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
+  // catch 404 and forward to error handler
+  app.use(function (req, res, next) {
   next(createError(404));
 });
 
