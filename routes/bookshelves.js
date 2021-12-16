751bb9218966c35465a8d3665e5848b8e017577b
jameschen56 <jameschen56@gmail.com>
2021-12-16 19:14:43 +0000

    working on shelves.js and bookshelves.pug

--- routes/bookshelves.js
+++ routes/bookshelves.js
@@ -6,10 +6,11 @@ const { loginUser, logoutUser } = require("../auth");
 
 const { csrfProtection, asyncHandler } = require('./utils');
 const db = require('../db/models');
+const Manga = require('../db/models/manga')
 
 router.get('/', csrfProtection, asyncHandler(async (req, res) => {
     const bookshelves = await db.Bookshelf.findAll({
-        include: db.Manga
+        include: [Manga]
     });
 
     res.render('bookshelves', { title: 'Bookshelves', bookshelves, csrfToken: req.csrfToken() });
